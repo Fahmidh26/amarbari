5953efd99e9d1060fe4979e3ac76c1f987a9a89c
Fahmid <fahmidh26@gmail.com>
2023-08-01 12:34:37 +0600

    4/22 Done Fahmid

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
         return view('admin.index');
     }
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+
+    public function AdminLogin(){
+
+        return view('admin.admin_login');
+
+    }// End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,10 @@ require __DIR__.'/auth.php';
 
 // ADMIN MIDDLEWARE
 Route::middleware(['auth','role:admin'])->group(function(){
+
     Route::get('/admin/dashboard', [AdminController::class , 'AdminDashboard'])->name('admin.dashboard');
+    
+    Route::get('/admin/logout', [AdminController::class , 'AdminLogout'])->name('admin.logout');
 });
 
 // END ADMIN MIDDLEWARE
@@ -47,5 +50,7 @@ Route::middleware(['auth','role:agent'])->group(function(){
 });
 
 // END AGENT MIDDLEWARE
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login'); 
 
 
