dae6966ba5b7a100d736483a05100fb2afd3513d
Fahmid <fahmidh26@gmail.com>
2023-08-03 16:49:37 +0600

    7 Start Fahmid

--- routes/web.php
+++ routes/web.php
@@ -119,6 +119,10 @@ Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.
             Route::get('/all/property', 'AllProperty')->name('all.property'); 
 
             Route::get('/add/property', 'AddProperty')->name('add.property');
+
+            Route::post('/store/property', 'StoreProperty')->name('store.property');
+
+            Route::get('/edit/property/{id}', 'EditProperty')->name('edit.property');
         
         });
 
