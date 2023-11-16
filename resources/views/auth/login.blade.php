8b78a868e57d979d758a52c3a8b4f06d69b5ac76
Fahmid <fahmidh26@gmail.com>
2023-11-16 13:08:28 +0600

    Slug Removed Done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,7 @@ User Login | AmarBari
         <div class="tab active-tab" id="tab-1">
             <div class="inner-box">
                 <h4>Sign in</h4>
-                <form action="{{ route('login') }}" method="post" class="default-form">
+                <form action="{{ route('agent.login') }}" method="post" class="default-form">
                @csrf
 
                     <div class="form-group">
