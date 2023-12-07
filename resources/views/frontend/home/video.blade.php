941dcb5a9762f13126e624c0302f9c7500a93e1c
ifazalam1 <ifaz.alam1@gmail.com>
2023-12-07 11:40:09 +0600

    video

--- resources/views/frontend/home/video.blade.php
+++ resources/views/frontend/home/video.blade.php
@@ -1,8 +1,8 @@
-<section class="video-section centred" style="background-image: url({{ asset('frontend/assets/images/background/video-1.jpg') }});">
+<section class="video-section centred" style="background-image: url({{ asset('frontend/assets/images/background/bari.png') }});">
     <div class="auto-container">
         <div class="video-inner">
             <div class="video-btn">
-                <a href="https://www.youtube.com/watch?v=nfP5N9Yc72A&amp;t=28s" class="lightbox-image" data-caption=""><i class="icon-17"></i></a>
+                <a href="https://www.youtube.com/watch?v=oX6Ttjlh38A&amp;t=28s" class="lightbox-image" data-caption="" data-autoplay="true"><i class="icon-17"></i></a>
             </div>
         </div>
     </div>
